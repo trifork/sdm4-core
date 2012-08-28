4ca77e3f011677ab136b536da9658423a9d99971
Jakob Færch <jrf@trifork.com>
2012-08-28 13:31:57 +0200

    Drop tabel(ler) mellem tests

--- src/test/java/dk/nsi/sdm4/core/persistence/migration/DbMigratorTest.java
+++ src/test/java/dk/nsi/sdm4/core/persistence/migration/DbMigratorTest.java
@@ -3,17 +3,25 @@ package dk.nsi.sdm4.core.persistence.migration;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.BadSqlGrammarException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.test.context.transaction.BeforeTransaction;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.sql.DataSource;
 import java.util.Date;
+
+import static org.junit.Assert.fail;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @Transactional
@@ -36,6 +44,11 @@ public class DbMigratorTest {
 	@Autowired
 	JdbcTemplate jdbcTemplate;
 
+	@BeforeTransaction
+	public void clearSchema() {
+		jdbcTemplate.update("DROP Table if exists " + DbMigrator.METADATA_TABLE_NAME);
+	}
+
 	@Test
 	public void youCanCallMigrateWithoutErrors() {
 		migrator.migrate();
@@ -43,7 +56,16 @@ public class DbMigratorTest {
 
 	@Test
 	public void metadataTableIsCreatedAutomatically() {
+		String selectSql = "SELECT max(installed_on) from " + DbMigrator.METADATA_TABLE_NAME;
+
+		try {
+			jdbcTemplate.queryForObject(selectSql, Date.class); // throws exception if table does not exist
+			fail("Metadata table should not exist before running the test"); // we expect the above to throw
+		} catch (BadSqlGrammarException ignored) {
+			// metadata table does not exist yet, continue the test
+		}
+
 		migrator.migrate();
-		jdbcTemplate.queryForObject("SELECT max(installed_on) from " + DbMigrator.METADATA_TABLE_NAME, Date.class); // throws exception if table does not exist
+		jdbcTemplate.queryForObject(selectSql, Date.class); // throws exception if table does not exist
 	}
 }
